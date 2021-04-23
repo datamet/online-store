b9b9d3a65987ac86f2babdb20c1b3ecb5136fa01
Mats Sommervold <mats@sommervold.net>
2021-04-23 20:12:02 +0200

    :recycle: Refactor typeloader to look in types directory

--- server/lib/validation/types.js
+++ server/lib/validation/types.js
@@ -1,19 +1,40 @@
 /**
  * Purpose: Load and parse all json types
  * 
- * Exports: An array of all types used by
+ * Exports: An object with all types used by
  * the rest api to validate requests
  */
 
 const fs = require("fs")
 const path = require("path")
-const log = require('../log')
+const error = require("../error")
+const log = require('../logger')
 
-let types = []
+let types = {}
+
+const validateType = type => {
+    if (!type.regex || !type.error) throw error.config()
+}
 
 try {
-    const typesFile = fs.readFileSync(path.resolve(__dirname, './types.json'))
-    types = JSON.parse(typesFile)
+    const typeFiles = fs.readdirSync(path.resolve(__dirname, './types'))
+    for (const filename of typeFiles) {
+        try {
+            const typeFile = fs.readFileSync(path.resolve(__dirname, `./types/${filename}`))
+            const newTypes = JSON.parse(typeFile)
+            for (const [name, newType] of Object.entries(newTypes)) {
+                if (Array.isArray(newType)) {
+                    newType.map(subtype => validateType(subType))
+                    continue
+                }
+                validateType(newType)
+            }
+            types = { ...types, ...newTypes }
+        }
+        catch (error) {
+            log(log.ERROR, `Could not load ${filename} type file`, { error })
+        }
+    }
 }
 catch (error) {
     log(log.ERROR, `Could not load types`, { error })
