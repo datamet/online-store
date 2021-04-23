c708fc4714d7d7776004318562c58c1939130149
Mats Sommervold <mats@sommervold.net>
2021-04-23 20:14:25 +0200

    :recycle: Refactor validator to support open/closed

--- server/lib/validation/validator.js
+++ server/lib/validation/validator.js
@@ -1,52 +1,44 @@
 const error = require('../error')
 const types = require('./types')
 
-const firstAndLast = (str, first, last) => {
-    if (str[0] === first && str[str.length - 1] === last) return true
-    else return false
+const formats = new Map()
+
+formats.set(
+	format => matchRegex('type', format) ? format.slice(1, -1) : false,
+	(type, value) => matchRegex(type, value)
+)
+
+formats.set(
+	format => matchRegex('type_array', format) ? format.slice(1, -1) : false,
+	(type, values) => {
+        if (!Array.isArray(values)) throw error.bad()
+        values.map(value => matchRegex(type, value))
+    }
+)
+
+formats.set(
+	format => matchRegex('path_variable', format) ? format.slice(1) : false,
+	(type, value) => matchRegex(type, value)
+)
+
+const matchRegex = (type, value) => {
+    t = types[type]
+    if (!t) throw error.config()
+	const matches = value.match(new RegExp(t.regex))
+	if (!matches) throw error.custom(400, t.error)
+	else return true
 }
 
-const matchRegex = (value, type) => {
-    const matches = value.match(new RegExp(type.regex))
-    if (!matches) throw error.custom(400, type.error)
-    else return true
-}
-
-const validate = (value, type) => {
-    if (Array.isArray(type)) {
-        for (const requirement of type) {
-            matchRegex(value, requirement)
+const validate = (format, value) => {
+    if (!format) throw error.config()
+    if (!value) throw error.bad()
+    if (typeof format === 'string') {
+        for (const [isFormat, validator] of formats) {
+            const matchesFormat = isFormat(format)
+            if (matchesFormat) return validator(matchesFormat, value)
         }
-        return true
     }
-    return matchRegex(value, type)
+    return format.toString() === value.toString()
 }
 
-const validator = (format = "", value) => {
-    if (firstAndLast(format, '[', ']')) {
-        value = value.toString()
-        const type = types[format.slice(1, -1)]
-        return validate(value, type)
-    }
-    else if (firstAndLast(format, '<', '>')) {
-        if (Array.isArray(value)) {
-            for (const element of value) {
-                const subFormat = format.replace('<', '[').replace('>', ']')
-                validator(subFormat, element)
-            }
-            return true
-        }
-        throw error.bad()
-    }
-    else if (format[0] === ':') {
-        value = value.toString()
-        const type = types[format.slice(1)]
-        return validate(value, type)
-    }
-    
-    value = value.toString()
-    const type = types[format]
-    return validate(value, type)
-}
-
-module.exports = validator
+module.exports = validate
