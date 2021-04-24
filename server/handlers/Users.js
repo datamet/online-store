402176a04dfdce618a8c4f4c80eee3978cfe5698
Mats Sommervold <mats@sommervold.net>
2021-04-24 20:33:18 +0200

    :bug: Fix old logger constant

--- server/handlers/Users.js
+++ server/handlers/Users.js
@@ -3,7 +3,7 @@ const log = require('../lib/logger')
 const Users = {}
 
 Users.createOne = (req, res, next) => {
-    log(debug.ACCEPT, `(200) ${req.path}`)
+    log(log.ACCEPT, `(200) ${req.path}`)
     res.send('ok')
     next()
 }
