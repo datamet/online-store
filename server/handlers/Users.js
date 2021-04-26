24f9251141e22dec7fed7453306a05e5910c4aa2
Mats Sommervold <mats@sommervold.net>
2021-04-26 18:33:57 +0200

    :recycle: Refactoring db import

--- server/handlers/Users.js
+++ server/handlers/Users.js
@@ -1,9 +1,9 @@
-const db = require('../lib/db/db')
+const { db } = require('../lib/db')
 
 const Users = {}
 
 Users.createOne = async (req, res, next) => {
-    await db.createUser(req.body.username)
+    await db.createUser({ username: req.body.username })
     res.send('ok')
     next()
 }
