0cfd06c1d9f4b2e39da56475feaf675968f0fb96
Mats Sommervold <mats@sommervold.net>
2021-04-29 05:13:41 +0200

    :building_construction: Moving matchesFrom module out of policies as it is a default policy

---
+++ server-base/router/matchesForm.js
@@ -0,0 +1,65 @@
+const error = require('../lib/error')
+const { validate } = require('../validation/validator')
+
+const setupMatchFrom = (req, res) => (form, matchedValue) => {
+    if (!matchedValue || typeof matchedValue !== 'object') throw error.missing()
+    for (const [key, meta] of Object.entries(form)) {
+        if (matchedValue[key]) {
+            let errorToThrow
+            let match = false
+            if (meta.keys) {
+                try {
+                    const matchFromRecursive = setupMatchFrom(req, res)
+                    matchFromRecursive(meta.keys, matchedValue[key])
+                    match = true
+                }
+                catch (error) {
+                    errorToThrow = error
+                }
+            }
+            else {
+                for (const format of meta.values) {
+                    try {
+                        validate(format, matchedValue[key])
+                        const policies = [...meta.policies]
+
+                        const next = (error) => {
+                            if (error) throw error
+                            const policy = policies.shift()
+                            if (policy) policy(req, res, next)
+                        }
+                        next()
+
+                        match = true
+                        break
+                    }
+                    catch (error) {
+                        errorToThrow = error
+                    }
+                }
+            }
+            if (!match) throw errorToThrow
+        }
+        else if (meta.optional) {
+            if (meta.default) matchedValue[key] = meta.default
+        }
+        else {
+            throw error.missing()
+        }
+    }
+}
+
+const matchesFrom = (form, toMatch) => (req, res, next) => {
+    const matchFormRecursive = setupMatchFrom(req, res)
+    
+    try {
+        const matchedValue = req[toMatch]
+        matchFormRecursive(form, matchedValue)
+    }
+    catch (error) {
+        next(error)
+    }
+    next()
+}
+
+module.exports = matchesFrom
