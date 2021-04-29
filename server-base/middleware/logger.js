3e19f6707b9273f3671d8ed5f576ff6964d5b30a
Mats Sommervold <mats@sommervold.net>
2021-04-29 13:59:10 +0200

    :adhesive_bandage: Add timeout to logger to prevent sending headers twice

--- server-base/middleware/logger.js
+++ server-base/middleware/logger.js
@@ -1,12 +1,14 @@
 const log = require('../lib/logger')
 
 const logger = (req, res, next) => {
-    if (res.headersSent) log(log.ACCEPT, `(${res.statusCode}) ${req.method} ${req.path}`)
-    else {
-        res.status(404).json({ error: `Cannot ${req.method} ${req.path}` })
-        log(log.REJECT, `(${res.statusCode}) ${req.method} ${req.path}`)
-    }
-    next()
+    setTimeout(() => {
+        if (res.headersSent) log(log.ACCEPT, `(${res.statusCode}) ${req.method} ${req.path}`)
+        else {
+            res.status(404).json({ error: `Cannot ${req.method} ${req.path}` })
+            log(log.REJECT, `(${res.statusCode}) ${req.method} ${req.path}`)
+        }
+        next()
+    }, 50)
 }
 
 module.exports = logger
