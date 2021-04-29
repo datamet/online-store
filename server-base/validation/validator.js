1a4d98c2c180e226bf3b2d2a3fdb067bcc8b54c2
Mats Sommervold <mats@sommervold.net>
2021-04-29 05:15:04 +0200

    :recycle: Refactor validator to take in types path

---
+++ server-base/validation/validator.js
@@ -0,0 +1,72 @@
+/**
+ * Exports: Validate function that matches a format
+ * with a value.
+ */
+
+const error = require('../lib/error')
+const typesloader = require('./types')
+let types = []
+
+// Map of accepted formats and how to validate them
+const formats = new Map()
+
+formats.set(
+	format => matchType('type', format, { noerror: true }) ? format.slice(1, -1) : false,
+	(type, value) => matchType(type, value)
+)
+
+formats.set(
+	format => matchType('type_array', format, { noerror: true }) ? format.slice(1, -1) : false,
+	(type, values) => {
+        if (!Array.isArray(values)) throw error.bad()
+        values.map(value => matchType(type, value))
+        return true
+    }
+)
+
+formats.set(
+	format => matchType('path_variable', format, { noerror: true }) ? format.slice(1) : false,
+	(type, value) => matchType(type, value)
+)
+
+const matchType = (type, value, options) => {
+    typeObj = types[type]
+    if (!typeObj) throw error.config()
+
+    if (Array.isArray(typeObj)) {
+        for (const requirement of typeObj) {
+            matchRegex(requirement, value, options)
+        }
+        return true
+    }
+    return matchRegex(typeObj, value, options)
+}
+
+const matchRegex = (type, value, options) => {
+    const noerror = options ? options.noerror : false
+	const matches = value.match(new RegExp(type.regex))
+	if (!matches) {
+        if (!noerror) throw error.custom(400, type.error)
+        else return false
+    }
+	else return true
+}
+
+// Exported validate function
+const validate = (format, value) => {
+    if (!format) throw error.config()
+    if (!value) throw error.bad()
+    if (typeof format === 'string') {
+        for (const [isFormat, validator] of formats) {
+            const matchesFormat = isFormat(format)
+            if (matchesFormat) return validator(matchesFormat, value)
+        }
+    }
+    return format.toString() === value.toString()
+}
+
+const setup = (types_path) => {
+    types = typesloader(types_path)
+}
+
+module.exports = { setup, validate }
