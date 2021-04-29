a893a9a9f02c0ef8e4067c40dab43b7ec469467e
Mats Sommervold <mats@sommervold.net>
2021-04-29 05:06:50 +0200

    :recycle: Move errorhandler module to base-server

---
+++ server-base/middleware/errorhandler.js
@@ -0,0 +1,22 @@
+/**
+ * Exports: Error handeling function that
+ * logs erors and sends errors back to the client
+ */
+const log = require('../lib/logger')
+
+const errorhandler = (error, req, res, next) => {
+	const status = error.status ? error.status : 500
+
+	log(log.REJECT, `(${status}) ${req.method.toUpperCase()} ${req.path}`, {
+		error,
+		status,
+	})
+
+	res.status(status).json(
+		status === 500
+			? { error: 'Internal server error' }
+			: { error: error.message }
+	)
+}
+
+module.exports = errorhandler
