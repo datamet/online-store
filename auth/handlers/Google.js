ee6742c386681d950db18c4540771e28375348eb
Mats Sommervold <mats@sommervold.net>
2021-05-02 10:31:44 +0200

    :passport_control: Create google auth handler

--- auth/handlers/Google.js
+++ auth/handlers/Google.js
@@ -1,25 +1,42 @@
-const { verify } = require('../lib/jwt')
+const { db } = require('../../server-base')
+const { error } = require('../../server-base')
+const hash = require('../lib/hash')
+const { verifyGoogleToken, authenticate, basic } = require('../lib/jwt')
 
 const Google = {}
 
-Google.authenticate = (req, res, next) => {
-    const id_token = req.query.id_token
-    const verifyToken = async () => {
-        try {
-            console.log(id_token)
-            const token = await verify(id_token)
-            console.log(token)
-            res.json(token)
-        }
-        catch(error) {
-            res.send('not ok')
-        }
-    }
-    verifyToken()
-}
-
-Google.unauthenticate = (req, res, next) => {
-    res.send('ok')
+Google.authenticate = async (req, res, next) => {
+	const id_token = req.query.id_token
+	const auth_header = req.header('Authorization')
+	const [ _email, password ] = basic(auth_header)
+	const token = await verifyGoogleToken(id_token)
+	const google_id = token.sub
+	const email = token.email
+	const email_verified = token.email_verified
+	const [first_name, last_name] = token.name
+	let user = await db.getUserByEmail({ email })
+	if (user && (!user.google_id || user.google_id !== google_id)) {
+		if (!password) {
+			throw error.custom(
+				409,
+				`A user with email: ${email} allready exists. Please confirm that it's yours.`,
+				{ email }
+			)
+		}
+		hash.compare(user.hash, password)
+	}
+	if (!user) {
+		user = await db.createUser({
+			google_id,
+			email,
+			email_verified,
+			first_name,
+			last_name,
+		})
+	}
+	await authenticate(user._id, res)
+	res.json({ message: 'Authenticated with google' })
+	next()
 }
 
 module.exports = Google
