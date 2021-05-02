a1b086df3c6a58a90148c88db787e836b39772ae
Mats Sommervold <mats@sommervold.net>
2021-05-02 13:28:48 +0200

    :recycle: Modified mongo-init script to use process.env

--- .docker/mongodb/initdb.d/mongo-init.js
+++ .docker/mongodb/initdb.d/mongo-init.js
@@ -1,20 +1,20 @@
 // use shell command to save env variable to a temporary file, then return the contents.
 // source: https://stackoverflow.com/questions/39444467/how-to-pass-environment-variable-to-mongo-script/60192758#60192758
-function getEnvVariable(envVar, defaultValue) {
-	var command = run(
-		'sh',
-		'-c',
-		`printenv --null ${envVar} >/tmp/${envVar}.txt`
-	)
-	// note: 'printenv --null' prevents adding line break to value
-	if (command != 0) return defaultValue
-	return cat(`/tmp/${envVar}.txt`)
-}
+// function getEnvVariable(envVar, defaultValue) {
+// 	var command = run(
+// 		'sh',
+// 		'-c',
+// 		`printenv --null ${envVar} >/tmp/${envVar}.txt`
+// 	)
+// 	// note: 'printenv --null' prevents adding line break to value
+// 	if (command != 0) return defaultValue
+// 	return cat(`/tmp/${envVar}.txt`)
+// }
 
 // create application user and collection
-var dbUser = getEnvVariable('DB_USER', 'regularuser')
-var dbPwd = getEnvVariable('DB_PWD', 'regularuser()')
-var dbName = getEnvVariable('DB_NAME', 'production')
+var dbUser = process.env.DB_USER || 'regularuser'
+var dbPwd = process.env.DB_PWD || 'regularpassword'
+var dbName = process.env.DB_NAME || 'production'
 db = db.getSiblingDB(dbName)
 
 // Create full access user and limited access user
@@ -44,7 +44,7 @@ db.createUser({
 	'roles': [
 		{
 			'role': 'dbOwner',
-			'db': getEnvVariable('DB_NAME', 'production'),
+			'db': process.env.DB_NAME || 'production',
 		},
 	],
 })
