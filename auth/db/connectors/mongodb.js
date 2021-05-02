8d89f1083100679bc9114ab762fc450b42d66341
Mats Sommervold <mats@sommervold.net>
2021-05-02 13:31:03 +0200

    :bug: Fix old import

--- auth/db/connectors/mongodb.js
+++ auth/db/connectors/mongodb.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb')
-const { db_protocol, db_uri, db_host, db_port, db_username, db_password, db_name } = require('../../../serverconfig')
+const { config } = require('../../../server-base')
 
-let uri = db_uri.replace('<protocol>', db_protocol)
-	uri = 	 uri.replace('<host>', db_host)
-	uri = 	 uri.replace('<port>', db_port)
-	uri = 	 uri.replace('<username>', db_username)
-	uri = 	 uri.replace('<password>', db_password)
+let uri = config.db_uri.replace('<protocol>', config.db_protocol)
+	uri = 	 uri.replace('<host>', config.db_host)
+	uri = 	 uri.replace('<port>', config.db_port)
+	uri = 	 uri.replace('<username>', config.db_username)
+	uri = 	 uri.replace('<password>', config.db_password)
 
 let db
 
@@ -14,7 +14,7 @@ const connect = async () => {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
-	db = client.db(db_name)
+	db = client.db(config.db_name)
 }
 
 const get = () => {
