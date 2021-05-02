20e140395a7b992e387860ece8bc7edc3f114d75
Mats Sommervold <mats@sommervold.net>
2021-05-02 22:48:07 +0200

    :recycle: Move server-base into packages and rename to server-framework

---
+++ packages/server-framework/middleware/logger.js
@@ -0,0 +1,14 @@
+const log = require('../lib/logger')
+
+const logger = (req, res, next) => {
+    setTimeout(() => {
+        if (res.headersSent) log(log.ACCEPT, `(${res.statusCode}) ${req.method} ${req.path}`)
+        else {
+            res.status(404).json({ error: `Cannot ${req.method} ${req.path}` })
+            log(log.REJECT, `(${res.statusCode}) ${req.method} ${req.path}`)
+        }
+        res.end()
+    }, 50)
+}
+
+module.exports = logger
---
+++ packages/server-framework/server.js
@@ -0,0 +1,48 @@
+/**
+ * Purpose: Creating server and telling it to use
+ * express on every request.
+ *
+ * Exports: Server object that can listen on any port
+ */
+
+const http = require('http')
+const express = require('express')
+const bodyparser = require('./middleware/bodyparser')
+const router = require('./router/router')
+const errorhandler = require('./middleware/errorhandler')
+const logger = require('./middleware/logger')
+const cookieparser = require('./middleware/cookieparser')
+
+const server = http.createServer()
+const app = express()
+
+server.on('request', app)
+
+const middlewareAfter = []
+const middlewareBefore = []
+const userMiddleware = middleware => (req, res, next) => {
+    let middlewareToUse = [...middleware]
+    
+    const nextUserMiddleware = (error) => {
+        if (error) return next(error)
+        const middleware = middlewareToUse.shift()
+        if (middleware) middleware(req, res, nextUserMiddleware)
+        else next()
+    }
+    nextUserMiddleware()
+}
+
+const useBefore = (func) => middlewareBefore.push(func)
+const useAfter = (func) => middlewareAfter.push(func)
+
+module.exports = (api_path, handlers_path, policies_path) => {
+    app.use(bodyparser)
+    app.use(cookieparser)
+    app.use(userMiddleware(middlewareBefore))
+    app.use(router(api_path, handlers_path, policies_path))
+    app.use(userMiddleware(middlewareAfter))
+    app.use(logger)
+    app.use(errorhandler)
+
+    return { server, useBefore, useAfter }
+}
