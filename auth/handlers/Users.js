48d6b90ff1d58e23ea5138b786b486fcfd3ac1bc
Mats Sommervold <mats@sommervold.net>
2021-05-02 10:32:34 +0200

    :passport_control: Create post /api/v1/users

--- auth/handlers/Users.js
+++ auth/handlers/Users.js
@@ -1,8 +1,36 @@
-const { db } = require('../../server-base')
+const path = require('path')
+const { db, error, validate } = require('../../server-base')
+const hasher = require('../lib/hash')
+const { basic } = require('../lib/jwt')
+
 const Users = {}
 
-Users.createOne = (req, res, next) => {
-    res.send('ok')
+Users.createOne = async (req, res, next) => {
+    const auth_header = req.header('Authorization')
+    const [ email, password ] = basic(auth_header)
+    validate('[email]', email)
+    validate('[password]', password)
+    const hash = hasher.create(password)
+    const first_name = req.body.first_name
+    const last_name = req.body.last_name
+
+    let user = await db.getUserByEmail({ email })
+    if (user) throw error.custom(409, "A user with that email allready exists")
+
+    user = await db.createUser({
+        email,
+        email_verified: false,
+        first_name,
+        last_name,
+        hash
+    })
+    
+    res.json({ message: 'User created', user_id: user._id })
+    next()
+}
+
+Users.login = (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, '../temp/index.html'))
     next()
 }
 
