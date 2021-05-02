18c56be121ef8971a433586f25e3658463cb5956
Mats Sommervold <mats@sommervold.net>
2021-05-02 10:32:06 +0200

    :passport_control: Create local auth handler

--- auth/handlers/Local.js
+++ auth/handlers/Local.js
@@ -1,12 +1,26 @@
-const { db } = require('../../server-base')
+const { db, error } = require('../../server-base')
+const { verifyLocalToken, authenticate, unauthenticate, basic } = require('../lib/jwt')
+const hash = require('../lib/hash')
 const Local = {}
 
-Local.authenticate = (req, res, next) => {
-    res.send('ok')
+Local.authenticate = async (req, res, next) => {
+    const auth_header = req.header('Authorization')
+    const [ email, password ] = basic(auth_header)
+    if (!email || !password) throw error.authentication()
+    const user = await db.getUserByEmail({ email })
+    if (!user || !user.hash) throw error.credentials()
+    hash.compare(user.hash, password)
+    await authenticate(user._id, res)
+    res.json({ message: 'Logged in' })
+    next()
 }
 
-Local.unauthenticate = (req, res, next) => {
-    res.send('ok')
+Local.unauthenticate = async (req, res, next) => {
+    const auth_token = req.getCookie('auth_token')
+    const token_info = await verifyLocalToken(auth_token)
+    await unauthenticate(token_info, res)
+    res.json({ message: 'Logged out' })
+    next()
 }
 
 module.exports = Local
