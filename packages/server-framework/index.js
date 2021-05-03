7aac131b7b31a985eb5f7a0c904181c0a9b36f4d
Mats Sommervold <mats@sommervold.net>
2021-05-03 18:42:32 +0200

    :recycle: Setup database and start with new config info

--- packages/server-framework/index.js
+++ packages/server-framework/index.js
@@ -4,29 +4,37 @@ const setupServer = require('./server')
 const listento = require('./start')
 const { log, setup: setupLogger } = require('./lib/logger')
 const error = require('./lib/error')
-const config = require('./serverconfig')
 
 const app = {}
 
 const server = ({ resources, config: configObj }) => {
-    const { events, errors, tracebacks } = configObj
-    setupLogger(events, errors, tracebacks)
-    validatorSetup(resources.types)
-    db.setup(resources.connectors, resources.gateways)
-    const { server: expressServer, useBefore, useAfter } = setupServer(resources.api, resources.handlers, resources.policies)
-    const { listen } = listento(expressServer, db.connect)
-    app.useBefore = useBefore
-    app.useAfter = useAfter
-    app.listen = listen
+	const { events, errors, tracebacks, gateway, autoport, port, mode } = configObj
+	setupLogger(events, errors, tracebacks)
+	validatorSetup(resources.types)
+	db.setup(gateway, resources.connectors, resources.gateways)
+	const { server: expressServer, useBefore, useAfter } = setupServer(
+		resources.api,
+		resources.handlers,
+		resources.policies
+	)
+	const { listen } = listento({
+        mode,
+        port,
+        autoport, 
+        server: expressServer, 
+        connect: db.connect
+    })
+	app.useBefore = useBefore
+	app.useAfter = useAfter
+	app.listen = listen
 
-    return app
+	return app
 }
 
 module.exports = {
-    server,
-    validate,
-    log,
-    db: db.db,
-    error,
-    config
+	server,
+	validate,
+	log,
+	db: db.db,
+	error
 }
