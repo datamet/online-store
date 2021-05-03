1019d0642cd4f855accfbcb2831043c9d50ed606
Mats Sommervold <mats@sommervold.net>
2021-05-03 21:24:06 +0200

    :recycle: Remove hash from users collection

--- auth-server/db/gateways/mongodb/users.js
+++ auth-server/db/gateways/mongodb/users.js
@@ -1,7 +1,8 @@
 const gateway = {}
 
-gateway.createUser = async (db, user) => {
-    const res = await db.collection('users').insertOne(user)
+gateway.createUser = async (db, { email, email_verified, username, hash }) => {
+    const res = await db.collection('users').insertOne({ email, email_verified, username })
+    await db.collection('hashes').insertOne({ user_id: res.insertedId, email, hash })
     return await db.collection('users').findOne({ _id: res.insertedId })
 }
 
