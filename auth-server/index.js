bc54087f9e21466040812e8af0f3958f534aabf6
Mats Sommervold <mats@sommervold.net>
2021-05-03 18:06:24 +0200

    :construction: Send config options to server framework

--- auth-server/index.js
+++ auth-server/index.js
@@ -1,15 +1,27 @@
 const path = require('path')
+const config = require('./config')
 const { server } = require('server-framework')
 
 const tokenvalidator = require('./middleware/tokenvalidator')
 
 const app = server({
-    api:        path.resolve(__dirname, './api'),
-    connectors: path.resolve(__dirname, './db/connectors'),
-    gateways:   path.resolve(__dirname, './db/gateways'),
-    handlers:   path.resolve(__dirname, './handlers'),
-    policies:   path.resolve(__dirname, './policies'),
-    types:      path.resolve(__dirname,'./types')
+    resources: {
+        api:        path.resolve(__dirname, './api'),
+        connectors: path.resolve(__dirname, './db/connectors'),
+        gateways:   path.resolve(__dirname, './db/gateways'),
+        handlers:   path.resolve(__dirname, './handlers'),
+        policies:   path.resolve(__dirname, './policies'),
+        types:      path.resolve(__dirname,'./types')
+    },
+    config: {
+        // mode,
+        // port,
+        // autoport,
+        // gateway,
+        events: 'server, mode, error, fatal, process, tips, accept, reject, database',
+        errors: 'all',
+        tracebacks: 'all'
+    }
 })
 
 app.useAfter(tokenvalidator)
