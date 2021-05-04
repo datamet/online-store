3a960cf83449c22471050b5656f15aa1177350e4
Mats Sommervold <mats@sommervold.net>
2021-05-04 12:31:33 +0200

    :sparkles: Create getUserGroups gateway

--- auth-server/db/gateways/mongodb/users.js
+++ auth-server/db/gateways/mongodb/users.js
@@ -1,7 +1,7 @@
 const gateway = {}
 
-gateway.createUser = async (db, { email, email_verified, username, hash }) => {
-    const res = await db.collection('users').insertOne({ email, email_verified, username })
+gateway.createUser = async (db, { email, email_verified, username, hash, groups }) => {
+    const res = await db.collection('users').insertOne({ email, email_verified, username, groups })
     await db.collection('hashes').insertOne({ user_id: res.insertedId, email, hash })
     return await db.collection('users').findOne({ _id: res.insertedId })
 }
@@ -12,6 +12,19 @@ gateway.getUserByEmail = async (db, { email }) => {
 
 gateway.getUserByGoogleId = async (db, { google_id }) => {
     return await db.collection('users').findOne({ google_id })
+}
+
+gateway.getUserGroups = async (db, { _id }) => {
+    const user = await db.collection('users').findOne({ _id })
+    return user.groups
+}
+
+gateway.userIsOwner = async (db, { user_id, _id }) => {
+    const res = await db.collection('users').find({
+        _id: user_id,
+        owns: { $in: [_id] },
+    }).count()
+    return res === 1
 }
 
 module.exports = gateway
