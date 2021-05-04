8683f948330fb37d0443a8580a7b8cdf5783e312
Mats Sommervold <mats@sommervold.net>
2021-05-04 12:38:20 +0200

    :sparkles: Implement inGroup policy

--- auth-server/policies/inGroup.js
+++ auth-server/policies/inGroup.js
@@ -1,9 +1,27 @@
-const setup = groups => (req, res, next) => {
+const { isOwner } = require('./isOwner')
+const { db, error } = require('server-framework')
+
+const setup = groups => async (req, res, next) => {
+    const userInGroup = await inGroup(req.user._id, groups, { params: req.params, query: req.query })
+    if (!userInGroup) {
+        throw error.unauthorized()
+    }
     next()
 }
 
-const inGroup = (user_id, groups) => {
+const inGroup = async (user_id, groups, options) => {
+    const params = options && options.params ? options.params : null
+    const query = options && options.query ? options.query : null
+    const entity = options && options.entity ? options.entity : null
 
+    const user_groups = await db.getUserGroups({ _id: user_id })
+    console.log("This user is in user group: ------------", user_groups)
+    let result = user_groups.some(group => groups.includes(group))
+    console.log(`User includes ${groups}: ${result}`)
+    if (!result && groups.includes('owner')) {
+        result = await isOwner(user_id, { params, query, entity })
+    }
+    return result
 }
 
 module.exports = { setup, inGroup }
