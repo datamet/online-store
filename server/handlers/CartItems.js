1f499a3e2e55a3eb54ac3868c2c5e9a49c78a15d
Erik Storås Sommer <eriksommer99@gmail.com>
2021-05-04 14:45:49 +0200

    :construction: Handlers and gateways for cart items. Needs more work

--- server/handlers/CartItems.js
+++ server/handlers/CartItems.js
@@ -3,19 +3,51 @@ const { db } = require('../lib/db')
 const CartItems = {}
 
 CartItems.createOne = async (req, res, next) => {
-
+	await db.createCartItem({
+		user_id: req.params.user_id,
+		product_id: req.body.product_id,
+		amount: req.body.amount
+	})
+	res.json({
+		message: 'Item cart created'
+	})
+	next()
 }
 
-CartItems.getOne = async (req, res, next) => {
-
+CartItems.getMultiple = async (req, res, next) => {
+	const cartItem = await db.getCartItems({ user_id: req.params.user_id })
+	res.json(cartItem)
+	next()
 }
 
 CartItems.updateOne = async (req, res, next) => {
-
+	await db.updateCartItem({
+		user_id: req.params.user_id,
+		product_id: req.params.product_id
+	})
+	res.json({
+		message: 'Updated cart item'
+	})
+	next()
 }
 
 CartItems.deleteOne = async (req, res, next) => {
+	await db.deleteCartItem({
+		user_id: req.params.user_id,
+		product_id: req.params.product_id
+	})
+	res.json({
+		message: 'Deleted cart item'
+	})
+	next()
+}
 
+CartItems.deleteMultiple = async (req, res, next) => {
+	await db.deleteCartItem({ _id: req.params.user_id })
+	res.json({
+		message: 'Deleted cart item'
+	})
+	next()
 }
 
 module.exports = CartItems
--- server/lib/db/gateways/mongodb/cartItems.js
+++ server/lib/db/gateways/mongodb/cartItems.js
@@ -1,19 +1,29 @@
 const gateway = {}
 
-gateway.createCartItem = async (db, product) => {
-	await db.collection('orders').insertOne(product)
+gateway.createCartItem = async (db, { user_id, product_id, amount }) => {
+	await db.collection('users').updateOne({ _id: user_id }, { $push: { cart: { product_id, amount } } })
 }
 
-gateway.getCartItems = async db => {
-	return await db.collection('orders').find().toArray()
+gateway.getCartItems = async (db, { _id }) => {
+	return await db.collection('users').find({ _id }).toArray()
 }
 
-gateway.updateCastItem = async (db, { _id }) => {
-	return await db.collection('orders').updateOne()
+gateway.updateCartItem = async (db, { user_id, product_id }) => {
+	return await db.collection('users').updateOne()
 }
 
-gateway.deleteCartItem = async (db, { _id }) => {
-	await db.collection('orders').deleteOne({ _id })
+gateway.deleteCartItem = async (db, { _id, product_id }) => {
+	const cart = db.collection('users').find({_id}, { cart: 1 })
+	await db.collection('products').updateOne({ _id: product_id }, { $inc: {amount: item.amount} })
+	await db.collection('users').updateOne({ _id }, { $pull: { cart: product_id } })
+}
+
+gateway.deleteCartItems = async (db, { _id }) => {
+	const items = await gateway.getCartItems(db, { _id })
+	for (const item of items) {
+		await db.collection('products').updateOne({ _id: item.product_id }, { $inc: {amount: item.amount} })
+	}
+	await db.collection('users').updateOne({ _id }, { cart: [] })
 }
 
 module.exports = gateway
