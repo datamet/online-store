35f4f66a6452b9889d6741d5b50446182b0f55b5
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-05 18:09:05 +0200

    :zap: Using server-framework Policies, checks if user is in group.

--- resource-server/policies/inGroup.js
+++ resource-server/policies/inGroup.js
@@ -1,8 +1,27 @@
-const error = require('../lib/error')
+const { isOwner } = require('./isOwner')
+const { db, error } = require('server-framework')
 
-const inGroup = groups => (req, res, next) => {
-    if (groups.indexOf("admin") !== -1) next(error.unauthorized())
-    else next()
+const setup = groups => async (req, res, next) => {
+    const userInGroup = await inGroup(req.user._id, groups, { params: req.params, query: req.query })
+    if (!userInGroup) {
+        throw error.unauthorized()
+    }
+    next()
 }
 
-module.exports = inGroup
+const inGroup = async (user_id, groups, options) => {
+    const params = options && options.params ? options.params : null
+    const query = options && options.query ? options.query : null
+    const entity = options && options.entity ? options.entity : null
+
+    const user_groups = await db.getUserGroups({ _id: user_id })
+    console.log("This user is in user group: ------------", user_groups)
+    let result = user_groups.some(group => groups.includes(group))
+    console.log(`User includes ${groups}: ${result}`)
+    if (!result && groups.includes('owner')) {
+        result = await isOwner(user_id, { params, query, entity })
+    }
+    return result
+}
+
+module.exports = { setup, inGroup }
