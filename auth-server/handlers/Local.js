6a85861253a809b7fb22ed552d9a410f2e48926e
Mats Sommervold <mats@sommervold.net>
2021-05-09 11:25:44 +0200

    :recycle: Add groups to new user

--- auth-server/handlers/Local.js
+++ auth-server/handlers/Local.js
@@ -1,5 +1,5 @@
 const { db, error } = require('server-framework')
-const { verifyLocalToken, authenticate, unauthenticate, basic } = require('../lib/jwt')
+const { verifyLocalToken, authenticate, unauthenticate, basic, clearAuthCookies } = require('../lib/jwt')
 const hash = require('../lib/hash')
 const Local = {}
 
@@ -10,17 +10,23 @@ Local.authenticate = async (req, res, next) => {
     const hash_info = await db.getHashByEmail({ email })
     if (!hash_info || !hash_info.hash) throw error.credentials()
     hash.compare(hash_info.hash, password)
-    await authenticate(hash_info.user_id, res)
+    await authenticate(hash_info.user_id, req, res)
     res.json({ message: 'Logged in', user_id: hash_info.user_id })
     next()
 }
 
 Local.unauthenticate = async (req, res, next) => {
     const auth_token = req.getCookie('auth_token')
-    const token_info = await verifyLocalToken(auth_token)
-    await unauthenticate(token_info, res)
-    res.json({ message: 'Logged out' })
-    next()
+    try {
+        const token_info = await verifyLocalToken(auth_token)
+        await unauthenticate(token_info, req, res)
+        res.json({ message: 'Logged out' })
+        next()
+    }
+    catch (error) {
+        await clearAuthCookies(req, res)
+        throw error
+    }
 }
 
 module.exports = Local
