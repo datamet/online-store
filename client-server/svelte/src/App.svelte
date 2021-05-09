7d8a7c187b6ea4abf08156559487c92f3717dff3
Mats Sommervold <mats@sommervold.net>
2021-05-09 23:48:30 +0200

    :construction: Create temp index file

--- client-server/svelte/src/App.svelte
+++ client-server/svelte/src/App.svelte
@@ -1,30 +1,9 @@
 <script>
-	export let name;
+	const host = location.hostname
+	const port = PORT || location.port
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Api is located at:</h1>
+	<h2>{host}:{port}</h2>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
