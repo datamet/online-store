d5757b5ad88775547da0f9c02b815542efcad597
Mats Sommervold <mats@sommervold.net>
2021-05-10 06:24:51 +0200

    :sparkles: Setup router

--- client-server/svelte/src/App.svelte
+++ client-server/svelte/src/App.svelte
@@ -1,9 +1,18 @@
 <script>
+	import { Router, Route } from 'svelte-routing'
+	import Frontpage from './containers/Frontpage.svelte'
+	import Profile from './containers/Profile.svelte'
 	const host = location.hostname
 	const port = PORT || location.port
+
+	let basepath = '/'
 </script>
 
 <main>
 	<h1>Api is located at:</h1>
 	<h2>{host}:{port}</h2>
+	<Router {basepath}>
+		<Route path="/profile" component="{Profile}"></Route>
+		<Route path="/" component="{Frontpage}"></Route>
+	</Router>
 </main>
