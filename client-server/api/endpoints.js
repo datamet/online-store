48716fcdc5be6ae93e6c5e05dfde787b3fc0e574
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-10 14:10:10 +0200

    :sparkles: Endpoints for orders and login

--- client-server/api/endpoints.js
+++ client-server/api/endpoints.js
@@ -5,23 +5,51 @@
  * backend api. Uses the client core to send requests
  */
 
-const routes = core => { return {
+import { setup as setupServer, browserFetch } from './fetch.js'
+import * as path from 'path'
+
+export const routes = {
 	// Login
-	login: (username, password) => core.post('/auth/users', { body: { username, password }})
+	login: (email, password) => {
+		const credentials = `${email}:${password}`
+		const encode = btoa(credentials)
+		const autHeader = `Basic ${encode}`
+		return browserFetch({
+			method: 'POST',
+			path: '/auth/local',
+			header: {
+				'Authorization': autHeader
+			}
+		})
+	},
 
 	// Orders
+	createOrder: body => browserFetch({ method: 'POST', path: '/api/v1/orders', body }),
+	getOrder: ({ order_id }) => browserFetch({ method: 'GET', path: `/api/v1/order/:${order_id}` }),
+	getOrders: ({ user_id }) => browserFetch({ method: 'GET', path: '/api/v1/orders', query: { user_id } }),
+	deleteOrder: ({ order_id }) => browserFetch({ method: 'DELETE', path: `/api/v1/order/:${order_id}` }),
 
 	// Products
+	createProduct: body => browserFetch({ method: 'POST', path: '/api/v1/products' }, body),
+	getProduct: ({ product_id }) => browserFetch({ method: 'GET', path: `/api/v1/product/:${product_id}` }),
+	getProducts: ({ index, count, keyword, search }) => browserFetch({
+		method: 'GET',
+		path: '/api/v1/products',
+		query: { index, count, keyword, search }
+	}),
+	updateProduct: ({ name, price, keywords, short_desc, long_desc, product_id }) => browserFetch({
+		method: 'POST',
+		path: `/api/v1/product/:${product_id}`,
+		body: { name, price, keywords, short_desc, long_desc }
+	}),
+	deleteProduct: ({product_id}) => browserFetch({method: 'DELETE', path: `/api/v1/product/:${product_id}`})
 
 	// Users
+
 
 	// Checkout
 
 	// Discounts
-
-
-}}
-
-export default (interactor) => {
-	return routes(interactor)
 }
+
+export const setup = setupServer
