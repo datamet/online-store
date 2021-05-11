8cd78ff0d7352f725d685260a8455aad967794af
Mats Sommervold <mats@sommervold.net>
2021-05-11 20:44:49 +0200

    :spakrles: Create nav component

---
+++ client-server/svelte/src/components/feature/Nav.svelte
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<nav>
+    <ul class="gap">
+        <slot name="left">
+
+        </slot>
+        <slot name="right">
+
+        </slot>
+    </ul>
+</nav>
+
+<style>
+    ul {
+        padding: 0;
+        margin: 0;
+        display: flex;
+        justify-content: space-between;
+        list-style-type: none;
+        align-items: center;
+    }
+</style>
