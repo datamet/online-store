8a67924c27a6cac393368559ae268abbe8e33e1b
Mats Sommervold <mats@sommervold.net>
2021-05-11 20:46:01 +0200

    :sparkles: Create formgroup component

---
+++ client-server/svelte/src/components/feature/form/FormGroup.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let center = false, flex = false
+</script>
+
+<div class="group" class:center class:flex>
+    <slot />
+</div>
+
+<style>
+    .group {
+        display: flex;
+        flex-direction: column;
+        gap: var(--gap);
+    }
+
+    .center {
+        align-items: center;
+    }
+
+    .flex {
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
