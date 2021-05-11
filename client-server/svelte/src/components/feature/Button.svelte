eb7828bfd5a4322e5646be22719ddc0504dc3114
Mats Sommervold <mats@sommervold.net>
2021-05-11 20:42:38 +0200

    :sparkles: Create button component

---
+++ client-server/svelte/src/components/feature/Button.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let action = () => {}, disabled = false, disabledText = ''
+</script>
+
+<div>
+    <button {disabled} on:click={action}><slot /></button>
+</div>
+
+<style>
+    button {
+        text-decoration: none;
+        color: var(--text-b);
+        background-color: var(--back-y);
+        padding: 1rem 1.5rem;
+        border-radius: .3rem;
+        border: none;
+        transition: all linear .2s;
+        display: flex;
+        align-items: center;
+        --gap: 1rem;
+    }
+
+    button:hover {
+        color: var(--text-g);
+        background-color: var(--control-y);
+    }
+
+    button:disabled:hover {
+        
+    }
+
+    button:disabled {
+        color: var(--text-l);
+        background-color: var(--back-g);
+    }
+</style>
