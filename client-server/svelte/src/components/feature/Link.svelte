fcbbb1cf7385eb473cc7313c5de4585f820f1fbe
Mats Sommervold <mats@sommervold.net>
2021-05-11 20:44:13 +0200

    :spakrles: Create link component

---
+++ client-server/svelte/src/components/feature/Link.svelte
@@ -0,0 +1,50 @@
+<script>
+    import { Link } from 'svelte-routing'
+
+    export let to = "/"
+    let path
+    $: selected = to === path
+
+    const updatePath = ({ location }) => {
+        path = location.pathname
+    }
+    
+</script>
+
+<div class="container">
+    <Link {to} getProps={updatePath}>
+        <div class="link gap" class:selected>
+            <slot>
+                Home
+            </slot>
+        </div>
+    </Link>
+</div>
+
+<style>
+    .container {
+        display: inline-block;
+    }
+
+    .container > :global(a) {
+        text-decoration: none;
+    }
+
+    .link {
+        height: 4rem;
+        text-decoration: none;
+        color: var(--text-b);
+        padding: 1rem;
+        border-radius: .3rem;
+        transition: all linear .2s;
+        display: flex;
+        align-items: center;
+        --gap: 1rem;
+    }
+
+    .link:hover,
+    .link.selected {
+        color: var(--text-p);        
+        background-color: var(--back-p);        
+    }
+</style>
