1ec80696e2e19b683125e2d3f797e4ba1e13d0eb
Mats Sommervold <mats@sommervold.net>
2021-05-12 12:42:53 +0200

    :iphone: Create continer layout

---
+++ client-server/svelte/src/components/layout/Container.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let contain = false, content = false, center = false
+</script>
+
+<div class="container" class:contain class:content class:center>
+    <slot />
+</div>
+
+<style>
+    .container {
+        margin: 0 auto;
+        padding: 0 var(--space);
+    }
+
+    .content {
+        padding: var(--space);
+    }
+
+    .contain {
+        max-width: 120rem;
+    }
+
+    .center {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
