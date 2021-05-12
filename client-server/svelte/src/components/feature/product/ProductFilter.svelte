c014d23128136ed09d631c860b9a401bab6ffcfb
Mats Sommervold <mats@sommervold.net>
2021-05-12 12:40:59 +0200

    :sparkles: Create product filter component

---
+++ client-server/svelte/src/components/feature/product/ProductFilter.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Form from '../form/Form.svelte'
+	import Search from '../form/Search.svelte'
+	import TagList from '../tags/TagList.svelte'
+</script>
+
+<Form>
+	<div class="controls">
+		<Search />
+		<TagList />
+	</div>
+</Form>
+
+<style>
+	.controls {
+		margin: 4rem 0 7rem 0;
+		border-radius: 0.3rem;
+		display: grid;
+		gap: 2rem;
+		align-items: center;
+	}
+</style>
