1b3aff7bee2d10edbba0864f7e8f98ff80085e06
Mats Sommervold <mats@sommervold.net>
2021-05-12 12:45:31 +0200

    :sparkles: Create user component

---
+++ client-server/svelte/src/containers/User.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Icon from '../components/feature/Icon.svelte'
+	import Link from '../components/feature/Link.svelte'
+</script>
+
+<div>
+	<Link to="/signin">
+		Sign in
+	</Link>
+</div>
+
+<style>
+	
+</style>
