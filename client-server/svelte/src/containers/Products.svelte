23364ae68ecbe65deac93f6bb948ce715229bd43
Mats Sommervold <mats@sommervold.net>
2021-05-12 12:44:23 +0200

    :sparkles: Creage products container

---
+++ client-server/svelte/src/containers/Products.svelte
@@ -0,0 +1,14 @@
+<script>
+    import ProductFilter from '../components/feature/product/ProductFilter.svelte';
+	import ProductList from '../components/feature/product/ProductList.svelte'
+    let search, keywords, index, count
+</script>
+
+<div>
+	<ProductFilter />
+	<ProductList />
+</div>
+
+<style>
+    
+</style>
