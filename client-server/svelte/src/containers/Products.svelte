8a89332cf2e1f03553dc73c80de89814c1a86232
Mats Sommervold <mats@sommervold.net>
2021-05-13 23:11:50 +0200

    :sparkles: Fetching products from backend

--- client-server/svelte/src/containers/Products.svelte
+++ client-server/svelte/src/containers/Products.svelte
@@ -1,12 +1,32 @@
 <script>
     import ProductFilter from '../components/feature/product/ProductFilter.svelte';
 	import ProductList from '../components/feature/product/ProductList.svelte'
+	import { productStore } from '../stores/fetch'
+	import { onMount } from 'svelte'
+
     let search, keywords, index, count
+
+	const fetchProducts = async () => {
+		const { set, res } = await productStore.fetch({ search, keyword: keywords, index, count })
+		if (res.body.products) set(res.body.products)
+	}
+
+	onMount(() => {
+		fetchProducts()
+	})
+
+	const handleSearch = async (frase, selectedKeywords) => {
+		search = frase
+		keywords = selectedKeywords
+		await fetchProducts()
+		return true
+	}
+
 </script>
 
 <div>
-	<ProductFilter />
-	<ProductList />
+	<ProductFilter search={handleSearch}/>
+	<ProductList products={$productStore} />
 </div>
 
 <style>
