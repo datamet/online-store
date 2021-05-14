07fc589fd208e6787b8feb3b7390b5d1bfe48f8b
Mats Sommervold <mats@sommervold.net>
2021-05-14 04:58:26 +0200

    :sparkles: Wire up to backend

--- client-server/svelte/src/containers/Signin.svelte
+++ client-server/svelte/src/containers/Signin.svelte
@@ -8,25 +8,23 @@
 	import FromText from '../components/feature/form/FromText.svelte'
 	import GoogleSignin from '../components/feature/GoogleSignin.svelte'
 	import FormGroup from '../components/feature/form/FormGroup.svelte'
-import Heading from '../components/type/Heading.svelte'
+	import Heading from '../components/type/Heading.svelte'
+	import { signin } from '../../../api/endpoints'
+	import { navigate } from 'svelte-routing'
 
+	let errorMessage = ''
 	let email, password
-	$: valid = email && password
 
-	const emailValidator = async email => {
-		const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-		return email.match(regex)
+	const handleSignin = async () => {
+		const res = await signin({ email, password })
+		if (res.body.message) navigate('/')
+		else if (res.body.error) errorMessage = res.body.error.message
+		else errorMessage = 'Something went wrong'
 	}
-
-	const passwordValidator = async email => {
-		return true
-	}
-
-	const signin = () => {}
 </script>
 
-<div>
-	<Container contain center>
+<Container section contain center>
+	<div>
 		<Form>
 			<FormGroup center>
 				<Logo size="medium"/>
@@ -37,31 +35,36 @@ import Heading from '../components/type/Heading.svelte'
 			</FormGroup>
 			<FormGroup>
 				<Input
+					required
 					bind:value={email}
 					id="signin-email"
-					validator={emailValidator}
 					type="email">Email</Input
 				>
 				<Input
+					required
 					bind:value={password}
 					id="signin-password"
-					validator={passwordValidator}
 					type="password">Password</Input
 				>
 			</FormGroup>
 			<FormGroup flex>
-				<Button action={signin} disabled={!valid}>Sign in</Button>
+				<Button action={handleSignin}>Sign in</Button>
 				<FromText>
 					<span>No account?</span>
 					<FormLink to="/signup">Sign up</FormLink>
 				</FromText>
 			</FormGroup>
+			<FormGroup>
+				<FromText error>{errorMessage}</FromText>
+			</FormGroup>
 		</Form>
-	</Container>	
-</div>
+	</div>
+</Container>	
 
 <style>
 	div {
-		--f-type: 'Abril Fatface'
+		--f-type: 'Abril Fatface';
+		max-width: 100%;
+		width: 25rem;
 	}
 </style>
