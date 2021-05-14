99aa0016c365191cd14653c523a8008cf3018b18
Erik Storås Sommer <erikssommer@gmail.com>
2021-05-14 15:08:09 +0200

    :sparkles: Handlers for validation

---
+++ resource-server/handlers/Validator.js
@@ -0,0 +1,13 @@
+const { db, error } = require('server-framework')
+
+const Validator = {}
+
+Validator.emailExist = async (req, res, next) => {
+	const email = req.body.email
+
+	const user = await db.getUserByEmail({ email })
+	if (user) throw error.custom(409, 'A user with that email already exists')
+
+}
+
+module.exports = Validator
