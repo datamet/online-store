e324f6722f9b82604cc879ad02a00d220e390f66
Mats Sommervold <mats@sommervold.net>
2021-05-16 18:53:04 +0200

    :sparkles: Create cart list

---
+++ client-server/svelte/src/components/feature/cart/CartList.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Heading from '../../type/Heading.svelte'
+    import Container from '../../layout/Container.svelte'
+    import CartItem from './CartItem.svelte'
+    export let cart = []
+
+    
+
+</script>
+
+<div class="list">
+    {#if cart && cart.length > 0}
+        {#each cart as item (item.product_id)}
+            <CartItem id={item.product_id} amount={item.amount}/>
+        {/each}
+    {:else}
+        <Container center>
+            <Heading type="h3">Cart is empty</Heading>
+        </Container>
+    {/if}
+</div>
+
+<style>
+    .list {
+        --f-size: 2.5rem;
+    }
+
+</style>
