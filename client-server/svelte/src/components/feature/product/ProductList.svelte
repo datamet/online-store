592996d4596a1e1fa8ef0f80a175ea7cc023ef34
Mats Sommervold <mats@sommervold.net>
2021-05-16 13:49:12 +0200

    :sparkles: Add currency to each product

--- client-server/svelte/src/components/feature/product/ProductList.svelte
+++ client-server/svelte/src/components/feature/product/ProductList.svelte
@@ -1,64 +1,15 @@
 <script>
 import Product from "./Product.svelte";
 
-    export let products = [
-        {
-            _id: "aldsjfivdojølvd",
-            name: "GoPro Hero 8 Black",
-            short_desc: "The newest gopro with style",
-            price: "$340.00"
-        },
-        {
-            _id: "dfasødjfio",
-            name: "AirTag",
-            short_desc: "The newest gopro with style",
-            price: "$8.00"
-        },
-        {
-            _id: "dfadfadsasødjfio",
-            name: "AirTag",
-            short_desc: "The newest gopro with style",
-            price: "$8.00"
-        },
-        {
-            _id: "dfasahghasfødjfio",
-            name: "AirTag",
-            short_desc: "The newest gopro with style",
-            price: "$8.00"
-        },
-        {
-            _id: "dfasø34tgfdjfio",
-            name: "AirTag",
-            short_desc: "The newest gopro with style",
-            price: "$8.00"
-        },
-        {
-            _id: "dfasødjagds3fio",
-            name: "AirTag",
-            short_desc: "The newest gopro with style",
-            price: "$8.00"
-        },
-        {
-            _id: "jagds3fio",
-            name: "AirTag",
-            short_desc: "The newest gopro with style",
-            price: "$8.00"
-        },
-        {
-            _id: "afdadfasødjagds3fio",
-            name: "AirTag",
-            short_desc: "The newest gopro with style",
-            price: "$8.00"
-        }
-    ]
+    export let products = []
 </script>
 
 <div class="list">
     {#each products as product (product._id)}
-        <Product>
+        <Product id={product._id}>
             <span slot="title">{product.name}</span>
             <span slot="description">{product.short_desc}</span>
-            <span slot="price">{product.price}</span>
+            <span slot="price">{product.currency ? product.currency : 'NOK'} {product.price},-</span>
         </Product>
     {/each}
 </div>
