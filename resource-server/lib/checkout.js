11e269d702a3cb73a04ac8cbd9b1e911d53c7cd5
Mats Sommervold <mats@sommervold.net>
2021-05-18 22:29:14 +0200

    :recycle: Throw error if checkout session not found

--- resource-server/lib/checkout.js
+++ resource-server/lib/checkout.js
@@ -3,6 +3,7 @@ const { db, error } = require('server-framework')
 const validateCheckoutSession = async (checkout_id, user_id) => {
     let session = await db.getAndDeleteCheckoutSession({ checkout_id })
     session = session.value
+    if (!session) throw error.exists()
     if (session.user && session.user !== user_id) throw error.unauthorized()
     if (Date.now() > session.exp) throw error.expired()
     if (!(session.user || session.exp || session.products)) throw error.unauthorized()
