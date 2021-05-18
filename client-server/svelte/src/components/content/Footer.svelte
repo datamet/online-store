e653a758482ff74ac59b1ee8db4f0afa265585b5
Mats Sommervold <mats@sommervold.net>
2021-05-18 22:37:37 +0200

    :sparkles: Create footer component

---
+++ client-server/svelte/src/components/content/Footer.svelte
@@ -0,0 +1,28 @@
+<script>
+	import Container from '../layout/Container.svelte'
+
+</script>
+
+<Container contain>
+	<footer>
+		<div>
+			<p class="copy">&copy; Sahara Store, 2021</p>
+		</div>
+	</footer>
+</Container>
+
+<style>
+	footer {
+		margin-top: 6rem;
+		padding-bottom: 4rem;
+		border-top: 1px solid var(--back-lg);
+	}
+
+	p {
+		color: var(--text-g);
+	}
+
+	.copy {
+		text-align: center;
+	}
+</style>
