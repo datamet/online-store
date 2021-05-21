6ebaf6058e57232535df0c4923b1462e90906d69
Mats Sommervold <mats@sommervold.net>
2021-05-21 23:44:08 +0200

    :sparkles: Create checkout product list

---
+++ client-server/svelte/src/components/feature/checkout/CheckoutProductList.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Container from '../../layout/Container.svelte'
+    import CheckoutProduct from './CheckoutProduct.svelte'
+    import Load from '../Load.svelte'
+
+    export let products, error, percent = 0
+</script>
+
+{#if products}
+    <ul>
+        {#each products as product (product._id)}
+            <li>
+                <CheckoutProduct {product} {percent} />
+            </li>
+        {/each}
+    </ul>
+{:else if !error}
+    <Container center section>
+        <Load />
+    </Container>
+{/if}
+
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+</style>
