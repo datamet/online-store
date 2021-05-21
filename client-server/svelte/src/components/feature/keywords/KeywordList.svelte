5eefb2dffaf5e1c4e817fbd243a8cb93477ab311
Mats Sommervold <mats@sommervold.net>
2021-05-21 23:50:35 +0200

    :sparkles: Create keyword list component

---
+++ client-server/svelte/src/components/feature/keywords/KeywordList.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Keyword from './Keyword.svelte'
+    import { createEventDispatcher } from 'svelte'
+
+    const dispatch = createEventDispatcher()
+
+	export let keywords = []
+
+	const remove = event => {
+        const keyword = event.detail.keyword
+        dispatch('remove', { keyword })
+		keywords.splice(keywords.indexOf(keyword), 1)
+		keywords = keywords
+	}
+</script>
+
+<ul class="gap">
+	{#each keywords as keyword (keyword)}
+		<li><Keyword {keyword} on:x={remove} removable /></li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		display: flex;
+		align-items: center;
+	}
+</style>
