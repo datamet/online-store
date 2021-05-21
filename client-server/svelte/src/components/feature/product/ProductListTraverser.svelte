373e6bee0a7a6280d406659b4da616da6a15fda2
Mats Sommervold <mats@sommervold.net>
2021-05-21 23:52:22 +0200

    :sparkles: Creat product list traverser

---
+++ client-server/svelte/src/components/feature/product/ProductListTraverser.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Button from "../Button.svelte";
+    export let previous, next, fetch
+</script>
+
+<div class="flex">
+    <Button disabled={previous ? false : true} action={() => fetch(previous)}>Previous</Button>
+    <Button disabled={next ? false : true} action={() => fetch(next)}>Next</Button>
+</div>
+
+<style>
+    .flex {
+        padding-top: 2rem;
+        border-top: 1px solid var(--back-lg);
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
