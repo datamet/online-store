777e312d70bee34cbc55b04288e42e54cfd99a1f
Mats Sommervold <mats@sommervold.net>
2021-05-23 04:20:57 +0200

    :card_file_box: Add ObjectId() to each id string

--- auth-server/db/gateways/mongodb/tokens.js
+++ auth-server/db/gateways/mongodb/tokens.js
@@ -1,8 +1,10 @@
+const ObjectId = require('mongodb').ObjectID
+
 const gateway = {}
 
 gateway.storeAuthToken = async (db, { user_id, auth_token, expires }) => {
-    await db.collection('tokens').insertOne({ user_id, auth_token, expires })
-    await db.collection('users').updateOne({ _id: user_id }, { $push: { auth_tokens: auth_token } })
+    await db.collection('tokens').insertOne({ user_id: ObjectId(user_id), auth_token, expires })
+    await db.collection('users').updateOne({ _id: ObjectId(user_id) }, { $push: { auth_tokens: auth_token } })
 }
 
 gateway.getAuthToken = async (db, { auth_token }) => {
@@ -10,8 +12,8 @@ gateway.getAuthToken = async (db, { auth_token }) => {
 }
 
 gateway.removeAuthToken = async (db, { user_id, auth_token }) => {
-    await db.collection('tokens').removeOne({ auth_token: auth_token })
-    await db.collection('users').updateOne({ _id: user_id }, { $pull: { auth_tokens: auth_token } })
+    await db.collection('tokens').removeOne({ auth_token })
+    await db.collection('users').updateOne({ _id: ObjectId(user_id) }, { $pull: { auth_tokens: auth_token } })
 }
 
 module.exports = gateway
